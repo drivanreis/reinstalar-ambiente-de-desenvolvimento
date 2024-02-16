a1c7a098dbe4efacc1dc654740b203f659b9b641
drivanreis <105787691+drivanreis@users.noreply.github.com>
2024-02-16 20:05:55 -0300

    Primeiros Passos

---
+++ script-Reinstalacao.sh
@@ -0,0 +1,10 @@
+sudo apt update 
+sudo apt upgrade
+sudo apt install gdebi -y
+sudo apt install ./arqDeb/code_1.86.2-1707854558_amd64.deb -y
+sudo apt install ./arqDeb/franz_5.10.0_amd64.deb -y
+sudo apt install ./arqDeb/GitHubDesktop-linux-3.1.1-linux1.deb -y
+sudo apt install ./arqDeb/google-chrome-stable_current_amd64.deb -y
+sudo apt install ./arqDeb/zoom_amd64.deb -y
+sudo snap install slack -y
+
